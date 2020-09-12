71d29955ae5be6a69c66ffced6730f92c41743be
Przemo12121 <49036565+Przemo12121@users.noreply.github.com>
2020-09-12 21:30:48 +0200

    re

--- IDprovider.py
+++ IDprovider.py
@@ -2,9 +2,9 @@ from MeetLangClasses.MeetLangDatabaseFile import UsersDatabase, AccessTokensData
 from MeetLangClasses.CommunicationStatus import CommunicationStatus
 from flask import Flask, request
 import json
-import google
+#import google
 #from google.oauth2 import id_token
-#from google.auth.transport import requests
+from google.auth.transport import requests
 
 app = Flask(__name__)
 
