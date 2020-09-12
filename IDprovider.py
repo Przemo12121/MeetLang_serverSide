5f9eb1396c523e7ebd2b907d4e51ffba865fe8d0
Przemo12121 <49036565+Przemo12121@users.noreply.github.com>
2020-09-12 21:33:07 +0200

    l

--- IDprovider.py
+++ IDprovider.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 import json
 #import google
 #from google.oauth2 import id_token
-from google.auth.transport import requests
+#from google.auth.transport import requests
 
 app = Flask(__name__)
 
